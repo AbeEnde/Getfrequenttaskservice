a1ea93fc7d03c024bac6a45fdefd2e131842478c
abeende <abeende@gmail.com>
2023-03-17 03:31:05 -0400

    update some class

--- src/main/java/com/example/demo/controller/ServiceController.java
+++ src/main/java/com/example/demo/controller/ServiceController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Service;
-import com.example.demo.entity.ServiceTimestamp;
 import com.example.demo.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
